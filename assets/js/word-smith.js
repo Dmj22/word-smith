3521cd2b0e9359a4922be21d7eecfb3bd68b451e
Your Name <98237529+Dmj22@users.noreply.github.com>
2022-02-25 16:50:59 -0500

    added function to generate button

--- assets/js/word-smith.js
+++ assets/js/word-smith.js
@@ -1,32 +1,13 @@
-
-   
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// an object of user choices that we will get from user
-
+function generatePassword() {
+  console.log()
+}
 
 // Write password to the #password input
-function genPassword() {
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 12;
-  var password = "";
-for (var i = 0; i <= passwordLength; i++) {
- var randomNumber = Math.floor(Math.random() * chars.length);
- password += chars.substring(randomNumber, randomNumber +1);
-}
-      document.querySelector("password").value = password;
-}
-
-function copyPassword() {
-var copyText = document.querySelector("password");
-copyText.select();
-document.execCommand("copy");  
-}
-
+function writePassword() {
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
